8bf344d4fc7a16f90e6890e3f8019ea76c812466
danplo1 <budweiser401@wp.pl>
2019-03-27 19:55:07 +0100

    MyFirstSimpleReactWithSpringBootProject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Film from "/.Film"
+
 
 class App extends Component {
+
+  state = 
+  {
+  data: [],
+  }
+
+  
+  componentDidMount() {
+    fetch('http://localhost:8080/api/cassetts/all')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ data })
+      } 
+    );     
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       {this.state.data.map(film => <Film info={film}/>)}
       </div>
     );
   }
